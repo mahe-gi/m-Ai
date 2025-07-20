1cf0863284f83583377e167466603495d67e3ee6
mahesh ch <chmahesh.dev@gmail.com>
2025-07-20 12:46:46 +0530

    design

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const main = document.getElementById("main");
 const newChat = document.getElementById("newChat");
 
-const inp = document.createElement("input");
+const inp = document.createElement("textarea");
 
 const askBtn = document.createElement("button");
 inp.setAttribute("type", "text");
 inp.setAttribute("id", "userInp");
+inp.placeholder = "ask something...";
 askBtn.setAttribute("id", "askbtn");
-askBtn.textContent = "ask";
+askBtn.textContent = "🔎";
+const inpWrapper = document.createElement("div");
+inpWrapper.setAttribute("id", "inpWrapper");
+const btnWrapper = document.createElement("div");
+btnWrapper.setAttribute("id", "btnWrapper");
+const wrapper = document.createElement("div");
+wrapper.setAttribute("id", "wrapper");
+
 newChat.addEventListener("click", () => {
-  main.append(inp, askBtn);
+  main.append(wrapper);
+  wrapper.append(inpWrapper, btnWrapper);
+  inpWrapper.append(inp);
+  btnWrapper.append(askBtn);
 });
 
 askBtn.addEventListener("click", () => {
@@ -25,7 +36,7 @@ function getAiData(inp) {
       method: "POST",
       headers: {
         "Content-Type": "application / json",
-        "X-goog-api-key": "your api key",
+        "X-goog-api-key": "AIzaSyCWOyU_5gt2kftgMrFkw6v2IwSg2-Cgb5s",
       },
       body: JSON.stringify({
         contents: [
@@ -47,6 +58,7 @@ function getAiData(inp) {
         aiAns: data.candidates[0].content.parts[0].text,
       };
       chatData.push(obj);
+      inp.value = "";
       displayChat(chatData);
     })
     .catch((err) => console.log(err));
@@ -57,11 +69,32 @@ div.setAttribute("id", "chatArea");
 main.append(div);
 
 function displayChat(chatData) {
+  const userWrapper = document.createElement("div");
+  userWrapper.setAttribute("id", "userWrapper");
   const user = document.createElement("p");
+  user.setAttribute("id", "userP");
+  const span = document.createElement("span");
+  span.setAttribute("id", "userSpan");
   const ai = document.createElement("p");
+  ai.setAttribute("id", "aiP");
+  const span1 = document.createElement("span");
+  span1.setAttribute("id", "aiSpan");
+
+  const chatDiv = document.createElement("div");
+  div.setAttribute("id", "chatDiv");
+
   chatData.forEach((el) => {
-    user.textContent = el.user;
-    ai.textContent = el.aiAns;
-    div.append(user, ai);
+    user.append(span);
+    ai.append(span1);
+    span.textContent = el.user;
+    span1.textContent = el.aiAns;
+    div.append(chatDiv);
+    userWrapper.append(user);
+    chatDiv.append(userWrapper, ai);
   });
 }
+
+inp.addEventListener("input", () => {
+  inp.style.height = "auto";
+  inp.style.height = inp.scrollHeight + "px";
+});
